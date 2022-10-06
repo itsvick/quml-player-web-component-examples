43b927237574350b344e8e6ee1333c675a05218d
Vivek Kasture <vivek_kasture@techjoomla.com>
2022-10-06 17:55:22 +0530

    Added angular app example

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {
+  constructor() {
+    (window as any).questionListUrl = 'https://staging.sunbirded.org/api/question/v1/list';
+  }
+ }
